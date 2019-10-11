c73ace335d2dde625d2c48e1d28302f4974813ca
Tanay <48762037+SparkyTS@users.noreply.github.com>
2019-10-11 17:32:43 +0530

    Update MainActivity.java

--- main/java/com/sparkyts/newsreader/MainActivity.java
+++ main/java/com/sparkyts/newsreader/MainActivity.java
@@ -65,7 +65,6 @@ public class MainActivity extends AppCompatActivity {
 
             pd.dismiss();
             pd.cancel();
-            System.out.println("Done");
         }
 
         @Override
